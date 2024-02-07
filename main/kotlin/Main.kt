c00d541a206ea3ea4ea6a9a42e5bf8e2a0f9ae99
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-02-07 08:12:03 +0300

    added kotlin file

---
+++ main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+   var fullName: String = "teresia wangui"
+    println(fullName)
+
+    var age: Byte = 21
+    println(age)
+
+    var phoneNumber = "+254759088952"
+    println(phoneNumber)
+
+    var weightInKg: Float = 52F
+    println(weightInKg)
+
+    var citizenStudentKenyan: Boolean = true
+    println(citizenStudentKenyan)
+
+
+}
